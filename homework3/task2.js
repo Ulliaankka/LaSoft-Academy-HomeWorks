b9dfd5701c8afd16b083b4735931c6a0b15996b2
Ulianka <ulliaankka@gmail.com>
2022-10-28 14:59:06 +0200

    finished hw2

--- homework3/task2.js
+++
@@ -1,8 +0,0 @@
-const sumOfArrayEl = arr => {
-    let sum = 0;
-    for ( let i = 0; i < arr.length; i++){
-        sum += arr[i];
-    }
-    return sum;
-}
-console.log(sumOfArrayEl([12, 34, 23, 56]));
--- homework3/task3.js
+++
@@ -1,4 +0,0 @@
-const splittedArray = str => {
-    str.splice(',');
-}
-console.log(splittedArray("Red, Green, Blue, 1, White, 3, 4, 5, 7"));
